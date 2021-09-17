c062abee67586f75692bfbfb2462247c100133cb
Andrew Mackay <andrewmackayd@gmail.com>
2021-09-17 13:26:49 +0100

    third commit main

--- models/game.py
+++ models/game.py
@@ -1,4 +1,4 @@
-from models.game import Game
+from models.player import Player
 
 class Game:
     def __init__(self):
@@ -7,3 +7,25 @@ class Game:
     # Write a game class that has a function that takes in the 2 players..
     # Compares their choices and returns the winning player. 
     # If it is a draw the player should be None type..
+
+    player1 = Player("Steve", "rock")
+    player2 = Player("Stan", "scissors")
+
+    def play_game(self):
+        if self.player1.player_choice == self.player2.player_choice:
+            print(f"{self.player1.player_name}, and {self.player2.player_name} both selected {self.player1.player_choice}.. It's a tie!")
+        elif self.player1.player_choice == "rock":
+            if self.player2.player_choice == "scissors":
+                print(f"Rock crushes scissors! {self.player1.player_name} wins! {self.player2.player_name} loses!")
+            else:
+                print(f"Paper covers rock! {self.player2.player_name} wins! {self.player1.player_name} loses..")
+        elif self.player1.player_choice == "paper":
+            if self.player2.player_choice  == "rock":
+                print(f"Paper covers rock! {self.player1.player_name} wins! {self.player1.player_name} loses..")
+            else:
+                print(f"Scissors cuts paper! {self.player2.player_name} wins! {self.player1.player_name} loses..")
+        elif self.player1.player_choice == "scissors":
+            if self.player2.player_choice == "paper":
+                print(f"Scissors cuts paper! {self.player1.player_name} wins! {self.player1.player_name} loses..")
+            else:
+                print(f"Rock crushes scissors! {self.player2.player_name} wins! {self.player1.player_name} loses..")
